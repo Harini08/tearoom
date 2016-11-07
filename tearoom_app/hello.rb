4548c3db38543d026f4ac892f19f3017228ad7eb
Harini Manivannan <harini.manivannan@gmail.com>
2016-11-07 17:50:22 +0000

    added get request for events and form

--- tearoom_app/hello.rb
+++ tearoom_app/hello.rb
@@ -7,3 +7,13 @@ end
 get('/form') do
   erb:form
 end
+
+get('/events') do
+  erb:events
+end
+
+
+# get - displays the form
+# post - process the data (store data here in the database)
+#      - use mongodb
+# redirect to new get (where it's displayed - result)
